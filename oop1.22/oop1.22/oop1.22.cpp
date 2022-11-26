f28dea9586ce1e79bcb43aec20192905f2342db9
VladimirShklyaev <107680299+VladimirShklyaev@users.noreply.github.com>
2022-11-26 14:17:46 +0300

    Update oop1.22.cpp

--- oop1.22/oop1.22/oop1.22.cpp
+++ oop1.22/oop1.22/oop1.22.cpp
@@ -12,10 +12,10 @@ private:
     char Surname[MAX][20];
     char Name[MAX][20];
     int Birthday[MAX][3];
-    int PhoneNum[MAX];
+    long long PhoneNum[MAX];
 
 public:
-    void Find(char* FindName, int key);    // Поиск записи о студенте по фамилии 
+    void Find(char* FindName);    // Поиск записи о студенте по фамилии 
     void GetInfo(int ID);    // Вывод записи о студенте. 
     void Create();        // Создание новой запись о студенте.
     StudGroup();
@@ -43,7 +43,7 @@ void StudGroup::Create()
             cin >> Surname[i];
             cout << "Имя ";
             cin>> Name[i];
-            cout<<"Дата рождения <Д М Г> : "<<endl;
+            cout<<"Дата рождения <День Месяц Год> : "<<endl;
             cin >> Birthday[i][0];
             cin >> Birthday[i][1];
             cin>> Birthday[i][2];
@@ -57,8 +57,6 @@ void StudGroup::Create()
 void StudGroup::Find(char* FindName, int key)
 {
     bool inf = 0;
-    if (key == 1)
-    {
         for (int i = 0; i < MAX; i++)
         {
             if (strcmp(FindName, Surname[i]) == 0)
@@ -67,19 +65,16 @@ void StudGroup::Find(char* FindName, int key)
                 inf = 1;
             }
         }
-    }
-
     if (inf == 0) cout<<"Совпадений не найдено"<<endl;
 }
 void StudGroup::GetInfo(int ID)
 {
-    if (ID > -1)
-    {
-        printf("\nНайдено совпадение \n", ID);
-        printf("Фамилия, Имя: %s %s \n", Surname[ID], Name[ID]);
-        printf("Дата рождения: %d.%d.%d \n", Birthday[ID][0], Birthday[ID][1], Birthday[ID][2]);
-        printf("Тел: %d \n", PhoneNum[ID]);
-    }
+        cout<<"Найдено совпадение"<<endl;
+        cout << "Фамилия Имя " << Surname[ID] << " " << Name[ID] << endl;
+        cout<<"Дата рождения: "<< Birthday[ID][0]<<"." << Birthday[ID][1]<<"." << Birthday[ID][2] << endl;
+        cout << "Тел: " << PhoneNum[ID] << endl;
+        cout << "=================================================="<<endl;
+
 }
 int main()
 {
@@ -88,7 +83,7 @@ int main()
     SetConsoleOutputCP(1251);
     StudGroup Group1;
 
-    int s = -1, ID;
+    int s = 0;
     char FindName[20];
 
 
@@ -104,7 +99,7 @@ int main()
 
         case 2: printf("Введите фамилию: ");
            cin>>FindName;
-            Group1.Find(FindName, 1); 
+            Group1.Find(FindName); 
             break;
         
         }
